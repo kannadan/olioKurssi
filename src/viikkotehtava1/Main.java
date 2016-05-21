1c76ce5c1964901a2c8d3cbcffcd85aff9a340b4
kannadan <joonas.sou@gmail.com>
2016-05-21 23:23:39 +0300

    Viikkotehtävä1 compleat

--- src/viikkotehtava1/Main.java
+++ src/viikkotehtava1/Main.java
@@ -1,11 +1,21 @@
 package viikkotehtava1;
 
+
 /**
  * Created by Joonas on 21.5.2016.
  */
 public class Main {
 
     public static void main(String[] args){
+        Kysely asiakas = new Kysely();
+        Pankkitili tili = new Pankkitili(asiakas.nimi, asiakas.numero, asiakas.summa);
+        tili.info();
+        tili.otto(150);
+        tili.info();
+        tili.talletus(133);
+        tili.info();
+        tili.otto(-100);
+        tili.otto(10000);
 
     }
 
--- src/viikkotehtava1/Pankkitili.java
+++ src/viikkotehtava1/Pankkitili.java
@@ -8,4 +8,70 @@ public class Pankkitili {
     private String tilinumero, omistaja;
     private double saldo;
 
+    public Pankkitili(String nimi, String tilinumero, double saldo){
+        this.tilinumero = tilinumero;
+        omistaja = nimi;
+        this.saldo = saldo;
+    }
+
+    public Pankkitili(String nimi, String tilinumero){
+        this.tilinumero = tilinumero;
+        omistaja = nimi;
+        this.saldo = 0;
+    }
+
+
+    public void setTilinumero(String tilinumero) {
+        this.tilinumero = tilinumero;
+    }
+
+    public void setOmistaja(String omistaja) {
+        this.omistaja = omistaja;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+
+    public String getTilinumero() {
+        return tilinumero;
+    }
+
+    public String getOmistaja() {
+        return omistaja;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void talletus(double raha){
+        if (raha > 0) {
+            saldo += raha;
+        }
+        else{
+            System.out.println("Ei voi tallettaa negatiivista summaa");
+        }
+    }
+
+    public void otto(double raha){
+        if (raha < 0){
+            System.out.println("Ei voi nostaa negatiivista summaa");
+        }
+        else if (saldo > raha) {
+            saldo -= raha;
+        }
+        else{
+           System.out.println("Tilillä ei ole tarpeeksi rahaa");
+        }
+    }
+
+    public void info(){
+        System.out.println("Tilinumero: " + tilinumero);
+        System.out.println("Tilin omistaja: " + omistaja);
+        System.out.println("Tilin saldo: " + saldo);
+        System.out.println("");
+    }
+
 }
