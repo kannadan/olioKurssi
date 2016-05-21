c15e4ecd86bf4cdf7885836a7a0dc1b265d07b88
kannadan <joonas.sou@gmail.com>
2016-05-21 17:33:50 +0300

    Started drill 4 and removed redundant listener

---
+++ src/Harjoitus4/Main.java
@@ -0,0 +1,14 @@
+package Harjoitus4;
+
+/**
+ * Created by Joonas on 21.5.2016.
+ */
+import Harjoitus3.Listener;
+public class Main {
+
+    private static Listener sc = new Listener();
+    public static void main(String[] args){
+        sc.getInt();
+    }
+
+}
