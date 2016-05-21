75786a41ec1cee58db8e33d41937f7e9381c9a48
kannadan <joonas.sou@gmail.com>
2016-05-21 22:47:09 +0300

    Started viikkotehtävä1

---
+++ src/viikkotehtava1/Main.java
@@ -0,0 +1,12 @@
+package viikkotehtava1;
+
+/**
+ * Created by Joonas on 21.5.2016.
+ */
+public class Main {
+
+    public static void main(String[] args){
+
+    }
+
+}
---
+++ src/viikkotehtava1/Pankkitili.java
@@ -0,0 +1,11 @@
+package viikkotehtava1;
+
+/**
+ * Created by Joonas on 21.5.2016.
+ */
+public class Pankkitili {
+
+    private String tilinumero, omistaja;
+    private double saldo;
+
+}
